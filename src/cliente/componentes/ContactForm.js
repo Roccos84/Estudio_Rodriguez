e2de8e61b2e22bf213c5bcef1735fcec894c2c71
RoccoSunerson <suro844@gmail.com>
2023-07-14 16:59:54 -0300

    ds

--- src/cliente/componentes/ContactForm.js
+++ src/cliente/componentes/ContactForm.js
@@ -18,23 +18,7 @@ function ContactForm() {
     };
 
 
-    async function postData(url = '', data = {}) {
 
-        const response = await fetch(url, {
-            method: 'POST', // *GET, POST, PUT, DELETE, etc.
-            mode: 'cors', // no-cors, *cors, same-origin
-            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: 'same-origin', // include, *same-origin, omit
-            headers: {
-                'Content-Type': 'application/json'
-                // 'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            redirect: 'follow', // manual, *follow, error
-            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-            body: JSON.stringify(data) // body data type must match "Content-Type" header
-        });
-        return response.json(); // parses JSON response into native JavaScript objects
-    }
 
     const handleSubmit = async (e) => {
 
@@ -43,10 +27,25 @@ function ContactForm() {
 
         try {
 
-            postData(url = endpoint, data = { formData })
-                .then(data => {
+            async function postData() {
+
+                const response = await fetch(endpoint, {
+                    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                    mode: 'cors', // no-cors, *cors, same-origin
+                    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+                    credentials: 'same-origin', // include, *same-origin, omit
+                    headers: {
+                        'Content-Type': 'application/json'
+                        // 'Content-Type': 'application/x-www-form-urlencoded',
+                    },
+                    redirect: 'follow', // manual, *follow, error
+                    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+                    body: JSON.stringify(formData) // body data type must match "Content-Type" header
+                }).then(data => {
                     console.log(data); // JSON data parsed by `data.json()` call
                 });
+                return response.json(); // parses JSON response into native JavaScript objects
+            }
 
             // const response = await fetch(endpoint, {
             //     method: 'POST',
